5ca13738d85627b5a860db5e1717a11858e68456
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-06-20 17:13:05 +0200

    fix square brackets

--- src/main/java/org/lessons/bestOfTheYear/controller/IndexController.java
+++ src/main/java/org/lessons/bestOfTheYear/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,19 +18,19 @@ public class IndexController {
     @GetMapping("/")
     public String home(Model model) {
         String name = "Paolo";
-       model.addAttribute("name", name);
-    return "index"; // Ritorno il nome del template index.html resources/templates
+        model.addAttribute("name", name);
+        return "index"; // Ritorno il nome del template index.html resources/templates
     }
 
     @GetMapping("/movies")
-    public String movies(Model model){
-        model.addAttribute("movies",getBestMovies());
+    public String movies(Model model) {
+        model.addAttribute("movies", getBestMovies());
         return "movies";
     }
 
     @GetMapping("/songs")
-    public String songs(Model model){
-        model.addAttribute("songs",getBestSongs());
+    public String songs(Model model) {
+        model.addAttribute("songs", getBestSongs());
         return "songs";
     }
 
@@ -39,18 +38,33 @@ public class IndexController {
     //- uno restituisce una lista di oggetti di tipo Movie - getBestMovies()
     //- l’altro restituisce una lista di oggetti di tipo Song - getBestSongs()
 
- private List<Movie>  getBestMovies(){
+    private String getBestMovies() {
         List<Movie> bestMovies = new ArrayList<>();
-        bestMovies.add(new Movie(1,"Pulp fiction"));
-        bestMovies.add(new Movie(2,"Castaway on the moon"));
-        return bestMovies;
- }
+        bestMovies.add(new Movie(1, "Pulp fiction"));
+        bestMovies.add(new Movie(2, "Castaway on the moon"));
 
-    private List<Song>  getBestSongs(){
+        List<String> movieTitles = new ArrayList<>();
+
+        for (Movie movie : bestMovies) {
+            movieTitles.add(movie.getTitle());
+        }
+
+        String songTitles = String.join(", ", movieTitles);
+        return songTitles;
+
+    }
+
+    private String getBestSongs() {
         List<Song> bestSongs = new ArrayList<>();
-        bestSongs.add(new Song(1,"Bohemian rhapsody "));
-        bestSongs.add(new Song(2,"No limit of stars"));
-        return bestSongs;
+        bestSongs.add(new Song(1, "Bohemian rhapsody "));
+        bestSongs.add(new Song(2, "No limit of stars"));
+        List<String> songTitles = new ArrayList<>();
+
+        for (Song song : bestSongs) {
+            songTitles.add(song.getTitle());
+        }
+        String movieTitles = String.join(", ", songTitles);
+        return movieTitles;
     }
 
 
